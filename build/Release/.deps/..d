5dcd4c9e929f0bdae223e81f14cf84a1c28020f4
dickeyxxx <jeff@dickeyxxx.com>
2016-02-15 15:44:38 -0800

    new output

---
+++ build/Release/.deps/..d
@@ -0,0 +1,1 @@
+cmd_. := LD_LIBRARY_PATH=/Users/jdickey/src/github.com/dickeyxxx/jeb/build/Release/lib.host:/Users/jdickey/src/github.com/dickeyxxx/jeb/build/Release/lib.target:$$LD_LIBRARY_PATH; export LD_LIBRARY_PATH; cd ../.; node lib/bootstrap.js
---
+++ build/Release/.deps/Release/obj.target/bootstrap.stamp.d
@@ -0,0 +1,1 @@
+cmd_Release/obj.target/bootstrap.stamp := touch Release/obj.target/bootstrap.stamp
